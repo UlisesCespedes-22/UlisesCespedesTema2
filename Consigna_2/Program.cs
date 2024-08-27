85eb8f2afa534304baa93fd2df324bc5f6af4b73
54222 <54222@MSI>
2024-08-26 21:24:52 -0300

    Agregar archivos de proyecto.

---
+++ Consigna_2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Consigna_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Ingrese el radio del cilindro");
+                var radio = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese altura del cilindro");
+                var altura = int.Parse(Console.ReadLine());
+
+                var volumen = Math.PI * Math.Pow(radio, 2) * altura;
+
+                var superficie = Math.PI * 2 * Math.Pow(radio, 2) * altura;
+
+                Console.WriteLine($"El volumen es: {volumen:N2}");
+                Console.WriteLine($"La superficie es: {superficie:N2}");
+
+                Console.WriteLine("desea agregar un nuevo cilindro?");
+                Console.WriteLine("S: CONTUNIAR");
+                Console.WriteLine("N: FINALIZAR");
+
+                char opcion = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                switch (opcion)
+                {
+                    case 'S':
+                        Console.WriteLine("CONTINUE");
+                        break;
+                    case 'N':
+                        Console.WriteLine("FIN");
+                        var FIN = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                } 
+            } while (true);
+        }
+    }
+}
---
+++ ConsoleApp1_consigna_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1_consigna_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ingrese el radio del cilindro");
+                var radio = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese altura del cilindro");
+                var altura = int.Parse(Console.ReadLine());
+
+                var volumen = Math.PI * Math.Pow(radio, 2) * altura;
+
+                var superficie = Math.PI * 2 * Math.Pow(radio, 2) * altura;
+
+                Console.WriteLine($"El volumen es: {volumen:N2}");
+                Console.WriteLine($"La superficie es: {superficie:N2}");
+            }
+            catch (Exception)
+            {
+
+                
+            }
+
+        }
+    }
+}
